79477ae526313b98e9245eff3fe35fdc2f2236c3
apocatastas <molchanova.a.a@gmail.com>
2025-02-27 15:37:06 +0300

    task 28.4.5

--- LifeSpot/Static/JS/testing.js
+++ LifeSpot/Static/JS/testing.js
@@ -1,2 +1,2 @@
-﻿let userName = prompt("Пожалуйста, введите ваше имя");
-alert(`Приветствуем, ${userName}. В вашем имени ${userName.length} символов`)
+﻿let elements = document.getElementsByTagName('*');
+alert(`Количество элементов на странице:  ${elements.length}`);
