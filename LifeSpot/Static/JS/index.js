38be789bdd0fe00fdfb38e3fa6975c27d154d783
apocatastas <molchanova.a.a@gmail.com>
2025-03-04 12:51:10 +0300

    task 28.6.4

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,9 +1,18 @@
-let age = prompt("Please enter your age:");
+let session = new Map();
+session.set("userAgent", window.navigator.userAgent)
+session.set("age", prompt("Please enter your age:"))
+if (session.get("age") >= 18) {
+    let startDate = new Date().toLocaleString();
 
-if (age >= 18) {
-    alert("Welcome on Lifespot! " + new Date().toLocaleString());
+    alert("Welcome on Lifespot! " + '\n' + "Time of visit: " + startDate);
+    session.set("startDate", startDate)
 }
 else {
     alert("Viewer discretion is advised for people under 18. Redirecting...");
     window.location.href = "http://www.google.com"
+    a = true + 20 + "name"
+}
+
+for (let result of session) {
+    console.log(result)
 }
