9463dfdc9a503dc439719df4f6f9b6c63d7e49e2
apocatastas <molchanova.a.a@gmail.com>
2025-03-18 10:39:52 +0300

    task 29.5.5

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,33 +1,22 @@
-﻿// Логирование сессии (объявлено через expression)
-let sessionLog = function logSession(session) {
-    // Вывод в консоль
+﻿let session = new Map();
+
+let sessionLog = function logSession() {
     for (let result of session) {
         console.log(result)
     }
 }
 
-// Обработка сессии (объявлено через declaration)
 function handleSession() {
-    // создадим объект Map для хранения сессии
-    let session = new Map();
-    // Сохраним UserAgent
+    session.set("startDate", new Date().toLocaleString())
     session.set("userAgent", window.navigator.userAgent)
-
-    // Запросим возраст пользователя и тоже сохраним
+}
+function checkAge() {
     session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
-
-    // Проверка на возраст и сохранение сессии
     if (session.get("age") >= 18) {
-        let startDate = new Date().toLocaleString();
-
-        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
-        session.set("startDate", startDate)
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
     }
     else {
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
         window.location.href = "http://www.google.com"
     }
-
-    // Теперь мы возвращаем объект сессии
-    return session;
 }
