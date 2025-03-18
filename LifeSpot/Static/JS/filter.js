0f1925e0f017c0fa400c625362bbb855fc54b342
apocatastas <molchanova.a.a@gmail.com>
2025-03-18 10:35:43 +0300

    task 29.5.4

--- LifeSpot/Static/JS/filter.js
+++ LifeSpot/Static/JS/filter.js
@@ -1,10 +1,9 @@
-﻿function filterContent(inputParseFunction) { // в этот параметр будет передана ФУНКЦИЯ парсинга пользовательского ввода
-
+﻿function filterContent() {
     let elements = document.getElementsByClassName('video-container');
 
     for (let i = 0; i <= elements.length; i++) {
         let videoText = elements[i].querySelector(".video-title").innerText;
-        if (!videoText.toLowerCase().includes(inputParseFunction() /*Переданная функция вызвана*/.toLowerCase())) {
+        if (!videoText.toLowerCase().includes(inputParseFunction().toLowerCase())) {
             elements[i].style.display = 'none';
         } else {
             elements[i].style.display = 'inline-block';
