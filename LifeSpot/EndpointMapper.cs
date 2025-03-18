0ce839dbfd271713d36034bf1a8afb33be1d4dfc
apocatastas <molchanova.a.a@gmail.com>
2025-03-18 12:33:31 +0300

    task 29.7.4

--- LifeSpot/EndpointMapper.cs
+++ LifeSpot/EndpointMapper.cs
@@ -59,6 +59,18 @@ namespace LifeSpot
 
                 await context.Response.WriteAsync(html.ToString());
             });
+
+            builder.MapGet("/about", async context =>
+            {
+                var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "about.html");
+
+                // Загружаем шаблон страницы, вставляя в него элементы
+                var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
+                    .Replace("<!--SIDEBAR-->", sideBarHtml)
+                    .Replace("<!--FOOTER-->", footerHtml);
+
+                await context.Response.WriteAsync(html.ToString());
+            });
         }
         
 
