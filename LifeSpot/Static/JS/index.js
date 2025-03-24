8abff742f3bf5db871fbc64f93cbac5e1d984862
apocatastas <molchanova.a.a@gmail.com>
2025-03-24 14:17:59 +0300

    task 30.1.11

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,18 +1,23 @@
-﻿let session = new Map();
+﻿let session = {
+    'startDate': new Date().toLocaleString(),
+    'userAgent': window.navigator.userAgent,
+    'userAge': prompt("Пожалуйста, введите ваш возраст?")
+}
 
-let sessionLog = function logSession() {
-    for (let result of session) {
-        console.log(result)
-    }
+let sessionLog = function () {
+    console.log('Начало сессии: ' + session.startDate)
+    console.log('Данные клиента: ' + session.userAgent)
+    console.log('Возраст пользователя: : ' + session.userAge)
 }
 
 function handleSession() {
     session.set("startDate", new Date().toLocaleString())
     session.set("userAgent", window.navigator.userAgent)
 }
+
+
 function checkAge() {
-    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
-    if (session.get("age") >= 18) {
+    if (session.userAge >= 18) {
         alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
     }
     else {
